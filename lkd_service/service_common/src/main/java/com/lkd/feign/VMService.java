cd6cdde5b8eafe8353d19cf7f7fa4f06e63886e8
yrl <1543785919@qq.com>
2022-11-07 19:10:18 +0800

    注释

--- lkd_service/service_common/src/main/java/com/lkd/feign/VMService.java
+++ lkd_service/service_common/src/main/java/com/lkd/feign/VMService.java
@@ -12,9 +12,19 @@ import java.util.List;
 @FeignClient(value = "vm-service",fallbackFactory = VmServiceFallbackFactory.class)
 public interface VMService{
 
+    /**
+     * 根据售货机编码查询单个售货机相关信息
+     * @param innerCode
+     * @return
+     */
     @GetMapping("/vm/info/{innerCode}")
     VmVO getVMInfo(@PathVariable String innerCode);
 
+    /**
+     * 根据售货机编码查询商品信息列表
+     * @param innerCode
+     * @return 商品列表
+     */
     @GetMapping("/vm/skuList/{innerCode}")
     List<SkuVO> getSkuListByInnerCode(@PathVariable String innerCode);
 
@@ -26,17 +36,23 @@ public interface VMService{
     @GetMapping("/sku/{skuId}")
     SkuVO getSku(@PathVariable String skuId);
 
+    /**
+     * 根据售货机编码+商品id 查询当前商品是否还有库存
+     * @param innerCode 售货机编号
+     * @param skuId 商品Id
+     * @return
+     */
 
     @GetMapping("/vm/hasCapacity/{innerCode}/{skuId}")
     Boolean hasCapacity(@PathVariable String innerCode,@PathVariable Long skuId);
 
     /**
      * 获取点位名称
-     * @param id
-     * @return
+     * @param nodeId 点位Id
+     * @return 点位名称
      */
     @GetMapping("/node/nodeName/{id}")
-    String getNodeName(@PathVariable Long id);
+    String getNodeName(@PathVariable Long nodeId);
 
 
 }
